25b59515600524746d06f34fcd18ac883a0ab53d
SanzharS1 <241565@astanait.edu.kz>
2025-01-03 11:37:18 +0500

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,44 @@
+import java.util.Objects;
+
+public class Animal {
+    private String name;
+    private String species;
+    private int age;
+
+    public Animal(String name, String species, int age) {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal: " + name + ", Species: " + species + ", Age: " + age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return age == animal.age && Objects.equals(name, animal.name) && Objects.equals(species, animal.species);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, species, age);
+    }
+}
+
